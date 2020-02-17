a1ba7d5dd35acc35f6bd1001904dc1f4033e1ee7
Justin <jlkellogg18@outlook.com>
2020-02-17 15:31:30 -0700

    setup five day forecast cards, some CSS for cards, need to link JS ajax return to five day

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,9 @@ $("#searchIcon").on("click", function(e) {
     city +
     "&appid=7413c3086c5dd2422988610e26a92cea&units=imperial";
 
-var li = $("<li>").append(city);
-var ul = $("<ul>").append(li)
-$("#searchHistory").prepend(ul);   
+  var li = $("<li>").append(city);
+  var ul = $("<ul>").append(li);
+  $("#searchHistory").prepend(ul);
 
   $.ajax({
     url: searchCriteria,
@@ -21,11 +21,45 @@ $("#searchHistory").prepend(ul);
     $(".temp").text("Temperature: " + r.main.temp + " degrees");
     $(".humidity").text("Humidity: " + r.main.humidity + "%");
     $(".wind").text("Wind Speed: " + r.wind.speed + " mph");
+    // $(".uv").text("UV Index: " + )
     $(".detailsBox").show();
-    //  var UV
+
+    var lat = r.coord.lat;
+    var lon = r.coord.lon;
+    var searchUV =
+      "http://api.openweathermap.org/data/2.5/uvi?appid=7413c3086c5dd2422988610e26a92cea&lat=" +
+      lat +
+      "&lon=" +
+      lon;
+
+    $.ajax({
+      url: searchUV,
+      method: "GET"
+    }).then(function(r) {
+      console.log(r);
+      var uvIndex = r.value;
+      $(".uv").text("UV Index: " + uvIndex);
+    });
+  });
+  var searchFiveDay =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=7413c3086c5dd2422988610e26a92cea&units=imperial";
+
+  $.ajax({
+    url: searchFiveDay,
+    dataType: "json",
+    method: "GET"
+  }).then(function(r) {
+    console.log(r);
+    var weather="";
+    var weatherList = r.list[0,1,2,3,4];
+    $.each(weatherList, function(weather){
+      $("<p>")
+      $("")
+    }
+     
+
+    })
   });
 });
-
-//store response in variable
-
-//display response in the html page
