f1629242f7420a20fc8310ab68588e364227e965
Justin Kellogg <jlkellogg18@outlook.com>
2020-02-21 16:44:34 -0700

    code refactor, back on track

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,16 @@ $("#searchIcon").on("click", function(e) {
     city +
     "&appid=7413c3086c5dd2422988610e26a92cea&units=imperial";
 
-var li = $("<li>").append(city);
-var ul = $("<ul>").append(li)
-$("#searchHistory").prepend(ul);   
+  var li = $("<li>").append(city);
+  var ul = $("<ul>").append(li);
+  $("#searchHistory").prepend(ul);
+  storeSearch();
+
+  function storeSearch() {
+    var searchHistory = [];
+    searchHistory.push("#searchHistory");
+    localStorage.setItem("search", city);
+  }
 
   $.ajax({
     url: searchCriteria,
@@ -21,11 +28,41 @@ $("#searchHistory").prepend(ul);
     $(".temp").text("Temperature: " + r.main.temp + " degrees");
     $(".humidity").text("Humidity: " + r.main.humidity + "%");
     $(".wind").text("Wind Speed: " + r.wind.speed + " mph");
+    // $(".uv").text("UV Index: " + )
     $(".detailsBox").show();
-    //  var UV
+
+    var lat = r.coord.lat;
+    var lon = r.coord.lon;
+    var searchUV =
+      "http://api.openweathermap.org/data/2.5/uvi?appid=7413c3086c5dd2422988610e26a92cea&lat=" +
+      lat +
+      "&lon=" +
+      lon;
+
+    $.ajax({
+      url: searchUV,
+      method: "GET"
+    }).then(function(r) {
+      console.log(r);
+      var uvIndex = r.value;
+      $(".uv").text("UV Index: " + uvIndex);
+    });
+  });
+  var searchFiveDay =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=7413c3086c5dd2422988610e26a92cea&units=imperial";
+
+  $.ajax({
+    url: searchFiveDay,
+    method: "GET"
+  }).then(function(r) {
+    console.log(r);
+    for (var i = 0; i < 5; i++) {
+      var date = JSON.stringify(r.list[i].dt_txt);
+      var temp = JSON.stringify(r.list[i].main.temp);
+      var humidity = JSON.stringify(r.list[i].main.humidity);
+      $(".card-body-" + i).append(date, temp, humidity);
+    }
   });
 });
-
-//store response in variable
-
-//display response in the html page
